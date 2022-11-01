18f9a5344d704229335f5e1123bd10472c1afc51
Anne Rayana <annerayanajasmin@gmail.com>
2022-11-02 02:21:47 +0700

    Rapihin View

--- src/resource.js
+++
@@ -1,9 +0,0 @@
-function dropdown() {
-    document.querySelector("#submenu").classList.toggle("hidden");
-    document.querySelector("#arrow").classList.toggle("rotate-0");
-  }
-  dropdown();
-
-  function openSidebar() {
-    document.querySelector(".sidebar").classList.toggle("hidden");
-  }
